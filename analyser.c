a0645476352562b09e37f492769c019a56303d98
sundayefeterry <info.techbro.set@gmail.com>
2023-04-15 08:19:07 -0700

    analyser is done

--- analyser.c
+++ analyser.c
@@ -1,21 +1,24 @@
 #include "shell.h"
 /**
- *analyser- checks if the function is built in.
+ *analyser - checks if the function is built in.
  *@command: tokeniz the user input.
  *@buffer: user inline drived function.
-
+ *
  *Return: 1 if success 0 if not successful
  */
+
 int analyser(char **command, char *buffer)
 {
-    if (command_builtin(command, buffer))
-    {
-        return (1);
-    }
-    else if (**command == '/')
-    {
-        execution(command[0], command);
-        return (1);
-    }
-    return (0);
+	int val = 1;
+
+	if (command_builtin(command, buffer))
+	{
+		return (val);
+	}
+	else if (**command == '/')
+	{
+		execution(command[0], command);
+		return (val);
+	}
+	return (0);
 }
