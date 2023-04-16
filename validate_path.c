0e121d2fa1eebd910fb5e4bb9e929db079541778
SundayEfeTerry <info.techbro.set@gmail.com>
2023-04-16 17:36:37 +0100

    another validation
    another validation

---
+++ validate_path.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+/**
+* validate_path - validates mypath.
+* @mypath: tokener
+* @command: user command
+* Return: mypath || failure: NULL
+*/
+char *validate_path(char **mypath, char *command)
+{
+	int p = 0;
+	char *print;
+
+	while (mypath[p])
+	{
+		print = append_path(mypath[p], command);
+		if (access(print, F_OK | X_OK) == 0 || access(print, F_OK | X_OK) == NULL)
+        {
+            return (print);
+        }
+		free(print);
+
+		p++;
+	}
+
+	return (NULL);
+}
