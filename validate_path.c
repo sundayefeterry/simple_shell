20cf58211ea06eca149ef95b3076f6a49a3500ce
sundayefeterry <info.techbro.set@gmail.com>
2023-04-16 09:42:11 -0700

    done

--- validate_path.c
+++ validate_path.c
@@ -1,22 +1,26 @@
 #include "shell.h"
+
 /**
-* validate_path - validates mypath.
-* @mypath: tokener
-* @command: user command
-* Return: mypath || failure: NULL
-*/
+ * validate_path - validates mypath.
+ * @mypath: tokener
+ * @command: user command
+ *
+ * Return: mypath || failure: NULL
+ */
+
 char *validate_path(char **mypath, char *command)
 {
 	int p = 0;
+
 	char *print;
 
 	while (mypath[p])
 	{
 		print = append_path(mypath[p], command);
-		if (access(print, F_OK | X_OK) == 0 || access(print, F_OK | X_OK) == NULL)
-        {
-            return (print);
-        }
+		if (access(print, F_OK | X_OK) == 0)
+		{
+			return (print);
+		}
 		free(print);
 
 		p++;
