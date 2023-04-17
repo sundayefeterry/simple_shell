c8a114bb78fde4a6c7b6640341a5df8e840eedf1
sundayefeterry <info.techbro.set@gmail.com>
2023-04-17 09:44:14 -0700

    new fix

--- command_builtin.c
+++ command_builtin.c
@@ -1,30 +1,30 @@
 #include "shell.h"
 
 /**
-* command_builtin - function to handle builtin function calls and executions.
-* print_env - prints commands || display stdout
-* @command: tokenizer.
-* @commandpath: stdin reading.
-*
-* Return: 1 done, 0 failed
-*/
+ * command_builtin - function to handle builtin function calls and executions.
+ * print_env - prints commands || display stdout
+ * @command: tokenizer.
+ * @commandpath: stdin reading.
+ *
+ * Return: 1 done, 0 failed
+ */
 int command_builtin(char **command, char *commandpath)
 {
-	struct standardbuiltin {"environment", "quit"};
+	struct standardbuiltin standardbuiltin =  {"environment", "quit"};
 
-    char standardbuiltin = standardbuiltin;
+	int vall = 1;
 
-    int vall = 1;
-
-	if (_strcmp(*command, standardbuiltin.environment) == NULL || _strcmp(*command, standardbuiltin.environment) == 0)
+	if (_strcmp(*command, standardbuiltin.environment) == 0)
 	{
-		print_env();
+		print_out();
 		return (vall);
 	}
-	else if (_strcmp(*command, standardbuiltin.quit) == NULL || _strcmp(*command, standardbuiltin.quit) == 0)
+
+	if (_strcmp(*command, standardbuiltin.quit) == 0)
 	{
-		exit_cmd(command, commandpath);
+		exit_shell(command, commandpath);
 		return (vall);
 	}
+
 	return (0);
 }
