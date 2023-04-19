6da6a92f60f425b5e2a43fe1b3de9494488671c1
KiellyE <ezekielimereti@gmail.com>
2023-04-18 23:21:31 -0700

    print fuction updated

---
+++ print_out.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+/**
+ * print_out - prints the env string to standard output
+ *
+ * Return: NULL
+ */
+void print_out(void)
+{
+	int y = 0;
+	char **env = environ;
+
+	while (env[y])
+	{
+		write(STDOUT_FILENO, (const void *)env[y], _strlen(env[y]));
+		write(STDOUT_FILENO, "\n", 1);
+		y++;
+	}
+}
