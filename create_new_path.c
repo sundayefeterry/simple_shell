f4480cd4b9fa6dec09da1f9c2fa25f42873182ac
SundayEfeTerry <info.techbro.set@gmail.com>
2023-04-23 20:35:53 +0100

    fix
    fix

--- create_new_path.c
+++ create_new_path.c
@@ -1,13 +1,20 @@
 #include "main.h"
+
 /**
  * create_new_path - integrate np[np] to input
+ * 
  * @command: user-specified input
  * @np: np of input
+ * 
  * Return: np on success or NULL
  */
+
 char *create_new_path(char *np, char *command)
 {
+	int val = 2;
+
 	size_t f = 0, s = 0;
+
 	char *bfr;
 
 	if (!command && !np)
@@ -16,25 +23,31 @@ char *create_new_path(char *np, char *command)
 		np = "";
 	}
 	
-	bfr = malloc(sizeof(char) * (_len_str(np) + _len_str(command) + 2));
+	bfr = malloc(sizeof(char) * (_len_str(np) + _len_str(command) + val));
 
-	if (!bfr)
+	if (bfr == NULL)
+	{
 		return (NULL);
+	}
+
 	while (np[f])
 	{
 		bfr[f] = np[f];
 		f++;
 	}
+
 	if (np[f - 1] != '/')
 	{
 		bfr[f] = '/';
 		f++;
 	}
+
 	while (command[s])
 	{
 		bfr[f + s] = command[s];
 		s++;
 	}
+
 	bfr[f + s] = '\0';
 
 	return (bfr);
